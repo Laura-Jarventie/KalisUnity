b2e5eed21e508dd8dd49866e5336df020a57dfec
unknown <laurajarventie@hotmail.com>
2024-04-17 19:49:03 +0300

    eka

---
+++ Assets/My assets/Scripts/kissa.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class kissa : MonoBehaviour
+{
+    public CharacterController controller;
+    public Animator animator;
+
+    private float verticalSpeed = 0f;
+	private float horizontalSpeed = 0f;
+    private float mouseMovement = 0f;
+    public int speed = 3;
+    public int mouseSpeed = 3;
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //eteen ja sivulle liikkuminen
+		verticalSpeed = Input.GetAxis("Vertical");
+		horizontalSpeed = Input.GetAxis("Horizontal");
+        Vector3 direction = new Vector3(horizontalSpeed, -1, verticalSpeed);
+
+        //hiirellä kääntyminen
+		mouseMovement += Input.GetAxis("Mouse X") * mouseSpeed;
+		transform.localRotation = Quaternion.Euler(0, mouseMovement, 0);
+		direction = transform.rotation * direction;
+
+        //tämä lopulta liikuttaa
+		controller.Move(direction * Time.deltaTime * speed);
+
+        //animaatiot
+		if(verticalSpeed != 0){
+			animator.SetBool("Walk", true);
+		}
+		else{
+			animator.SetBool("Walk", false);
+		}
+    }
+}
